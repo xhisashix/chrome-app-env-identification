4fa71c3772b01652058a80e2b26ef2c8d493b762
Hisashi Iwamoto <hisasi622@gmail.com>
2023-12-16 17:12:27 +0900

    Add fixed position class and implement background change functionality

--- src/ts/background.ts
+++ src/ts/background.ts
@@ -1,3 +1,96 @@
+import Storage from "./storageClass";
+
+interface envSettings {
+  envName: string;
+  envUrl: string;
+  message: string;
+}
+
 chrome.action.onClicked.addListener(function () {
   chrome.runtime.openOptionsPage();
 });
+
+const storage = new Storage();
+
+// tabがアクティブになった時のイベント
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      getEnvSettings(tabs[0].url as string);
+    });
+  });
+});
+
+// get env settings from storage
+async function getEnvSettings(currentTabUrl: string) {
+  await storage.getStorage("env_settings", function (result: string) {
+    const envSettings = getEnvSettingsArray(result);
+    const tabUrl = currentTabUrl;
+    const envUrl = getEnvUrl(envSettings, tabUrl);
+    if (envUrl) {
+      console.log("ON");
+      chrome.action.setBadgeText({ text: "ON" });
+      chrome.action.setBadgeBackgroundColor({ color: "#4688F1" });
+
+      // 背景色を変更するためのコンテンツスクリプトを実行
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tabId = tabs[0]?.id || 0;
+        chrome.scripting.executeScript({
+          target: { tabId },
+          files: ["/js/content.js"],
+        });
+      });
+    } else {
+      console.log("OFF");
+      chrome.action.setBadgeText({ text: "OFF" });
+      chrome.action.setBadgeBackgroundColor({ color: "#FF0000" });
+    }
+  });
+}
+
+// get env url
+function getEnvUrl(envSettings: envSettings[], tabUrl: string): string {
+  let envUrl = "";
+  for (let i = 0; i < envSettings.length; i++) {
+    if (checkIncludeUrl(envSettings[i].envUrl, tabUrl)) {
+      envUrl = envSettings[i].envUrl;
+      console.log(envUrl);
+      break;
+    }
+  }
+  return envUrl;
+}
+
+// get env settings from storage
+function getEnvSettingsArray(result: string): envSettings[] {
+  if (result) {
+    return JSON.parse(result);
+  } else {
+    return [];
+  }
+}
+
+// check include env url
+function checkIncludeUrl(envUrl: string, tabUrl: string) {
+  if (tabUrl === undefined) {
+    return false;
+  }
+  return tabUrl.includes(envUrl);
+}
+
+// ページの背景を変更する
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      getEnvSettings(tabs[0].url as string);
+    });
+  }
+});
+
+// ページの背景を変更するelementを作成する
+function createChangeBackgroundElement() {
+  const changeBackgroundElement = document.createElement("div");
+  changeBackgroundElement.setAttribute("id", "change_background");
+  changeBackgroundElement.setAttribute("style", "background-color: red;");
+  document.body.appendChild(changeBackgroundElement);
+}
