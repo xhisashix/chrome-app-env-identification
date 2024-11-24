ed5222e7f004e6f8cf381ff28587be35a05016db
Hisashi Iwamoto <hisasi622@gmail.com>
2024-11-24 09:48:59 +0900

    Refactor content script to label style

--- src/ts/content.ts
+++ src/ts/content.ts
@@ -1,14 +1,11 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.envSettings) {
     const envSettings = request.envSettings;
-
     const projectName = envSettings.projectName || "";
     const envName = envSettings.envName || "";
     const message = envSettings.message || "備考なし";
     const color = envSettings.color || "red";
-    const labelPosition = createLabelPosition(
-      envSettings.labelPosition || "button"
-    );
+    const labelPosition = envSettings.labelPosition || "bottom";
     const activeFlag = envSettings.activeFlag || false;
     if (!activeFlag) {
       return;
@@ -29,6 +26,14 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   }
 });
 
+/**
+ * applyTestEnvironmentIndicator - テスト環境のラベルを適用する
+ * @param projectName - プロジェクト名
+ * @param envName - 環境名
+ * @param message - メッセージ
+ * @param color - 背景色
+ * @param labelPosition - ラベルの位置
+ */
 function applyTestEnvironmentIndicator(
   projectName: string,
   envName: string,
@@ -36,20 +41,23 @@ function applyTestEnvironmentIndicator(
   color: string,
   labelPosition: string
 ): void {
+  // すでにラベルが存在している場合は削除
+  const existingLabel = document.getElementById("test-environment-indicator");
+  if (existingLabel) {
+    existingLabel.remove();
+  }
+
   const messageDiv = document.createElement("div");
   messageDiv.id = "test-environment-indicator";
   messageDiv.innerHTML = `【${projectName}】${envName} : ${message}`;
   const style = document.createElement("style");
+  const labelPositionStyle = createLabelPosition(labelPosition);
   style.innerHTML = `
     #test-environment-indicator {
       position: fixed;
       width: 100vw;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      right: 0;
       margin: auto;
-      ${labelPosition}
+      ${labelPositionStyle}
       padding: 10px;
       background-color: ${color};
       color: white;
